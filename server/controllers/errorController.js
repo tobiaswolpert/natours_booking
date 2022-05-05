89bb126ba3002a899e799202afea88cbd598e798
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-05-05 15:44:44 -0400

    Added Error handling for invalid ID, duplicate fields and validation errors

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,8 +1,69 @@
+const AppError = require("../utils/appError");
+
+//MongoDB error handler for wrong ID
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Tour name ${err.keyValue.name} is already in use`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const message = Object.keys(err.errors)
+    .map((el) => err.errors[el].message)
+    .join(". ");
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    //Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error("Error", err);
+
+    // 2) Send generic message
+    res.status(500).json({ status: "error", message: "Something went wrong" });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    console.log("Error", error);
+    if (err.name === "CastError") {
+      error = handleCastErrorDB(error);
+    }
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
+    }
+    if (err.name === "ValidationError") {
+      error = handleValidationErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
+  }
 };
--- server/model/tourModel.js
+++ server/model/tourModel.js
@@ -24,7 +24,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a difficulty"],
       enum: {
         values: ["easy", "medium", "difficult"],
-        message: "difficulty is either easy, medium or difficult",
+        message: "Difficulty is either easy, medium or difficult",
       },
     },
     ratingsAverage: {
