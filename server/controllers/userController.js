2c314367b29d9efaf813fc454601b9f3c2d122a6
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-05-09 11:15:47 -0400

    Implemented user login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,12 @@
-exports.getAllUsers = (req, res) => {
+const User = require("../model/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
   res
-    .status(500)
-    .json({ status: "error", message: "This route is not yet defined" });
-};
+    .status(200)
+    .json({ status: "success", results: users.length, data: { users } });
+});
 
 exports.createUser = (req, res) => {
   res
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const authenticationController = require("../controllers/authenticationController");
 
 router.route("/signup").post(authenticationController.signup);
+router.route("/login").post(authenticationController.login);
 
 router
   .route("/")
