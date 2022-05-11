fc64505bf5c3a3e3786589aacba2476bd3eec476
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-05-11 15:02:38 -0400

    Implemented data sanitization

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -10,20 +13,35 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // 1) GLOBAL Middlewares
+//SET SECURITY HTTP headers
+app.use(helmet());
+
+//DEVELOPMENT logging with morgan middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+//LIMIT Requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
-
 app.use("/api", limiter);
-app.use(express.json());
+
+//BODY parser, reading data from the body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+//Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data Sanitization against XSS
+app.use(xss());
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
