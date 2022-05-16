e00ea0339938e0069be68aae08a78c174a5d1483
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-05-16 16:45:40 -0400

    Implemented unique index for user and tour to prevent duplicate reviews by the same user

--- server/model/tourModel.js
+++ server/model/tourModel.js
@@ -33,6 +33,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, "Rating must be above 1.0"],
       max: [5, "Rating must be below 5"],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, "A tour must have a price"] },
